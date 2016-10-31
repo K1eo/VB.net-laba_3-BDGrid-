089f86430e116a0c9369e62870ec8ceddab25a2c
K1eo <jitarju-kleo@rambler.ru>
2016-10-31 20:45:11 +0300

    Add files via upload

---
+++ FAdd.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FAdd
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TB1 = New System.Windows.Forms.TextBox()
+        Me.TB2 = New System.Windows.Forms.TextBox()
+        Me.TB3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Add2 = New System.Windows.Forms.Button()
+        Me.Remove2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TB1
+        '
+        Me.TB1.Location = New System.Drawing.Point(123, 12)
+        Me.TB1.Name = "TB1"
+        Me.TB1.Size = New System.Drawing.Size(159, 20)
+        Me.TB1.TabIndex = 0
+        '
+        'TB2
+        '
+        Me.TB2.Location = New System.Drawing.Point(123, 53)
+        Me.TB2.Name = "TB2"
+        Me.TB2.Size = New System.Drawing.Size(159, 20)
+        Me.TB2.TabIndex = 1
+        '
+        'TB3
+        '
+        Me.TB3.Location = New System.Drawing.Point(123, 94)
+        Me.TB3.Name = "TB3"
+        Me.TB3.Size = New System.Drawing.Size(159, 20)
+        Me.TB3.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Coefficients"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(12, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(12, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Color"
+        '
+        'Add2
+        '
+        Me.Add2.BackColor = System.Drawing.Color.Black
+        Me.Add2.ForeColor = System.Drawing.Color.Transparent
+        Me.Add2.Location = New System.Drawing.Point(10, 138)
+        Me.Add2.Name = "Add2"
+        Me.Add2.Size = New System.Drawing.Size(126, 33)
+        Me.Add2.TabIndex = 12
+        Me.Add2.Text = "Add"
+        Me.Add2.UseVisualStyleBackColor = False
+        '
+        'Remove2
+        '
+        Me.Remove2.BackColor = System.Drawing.Color.Black
+        Me.Remove2.ForeColor = System.Drawing.Color.White
+        Me.Remove2.Location = New System.Drawing.Point(156, 138)
+        Me.Remove2.Name = "Remove2"
+        Me.Remove2.Size = New System.Drawing.Size(126, 33)
+        Me.Remove2.TabIndex = 12
+        Me.Remove2.Text = "Отмена"
+        Me.Remove2.UseVisualStyleBackColor = False
+        '
+        'FAdd
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(290, 186)
+        Me.Controls.Add(Me.Remove2)
+        Me.Controls.Add(Me.Add2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TB3)
+        Me.Controls.Add(Me.TB2)
+        Me.Controls.Add(Me.TB1)
+        Me.Name = "FAdd"
+        Me.Text = "FAdd"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TB1 As TextBox
+    Friend WithEvents TB2 As TextBox
+    Friend WithEvents TB3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Add2 As Button
+    Friend WithEvents Remove2 As Button
+End Class
---
+++ Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Access
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGrid1 = New System.Windows.Forms.DataGridView()
+        Me.Add1 = New System.Windows.Forms.Button()
+        Me.Delete1 = New System.Windows.Forms.Button()
+        Me.Open1 = New System.Windows.Forms.Button()
+        CType(Me.DataGrid1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGrid1
+        '
+        Me.DataGrid1.AllowUserToAddRows = False
+        Me.DataGrid1.AllowUserToDeleteRows = False
+        Me.DataGrid1.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.DataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGrid1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGrid1.Name = "DataGrid1"
+        Me.DataGrid1.ReadOnly = True
+        Me.DataGrid1.Size = New System.Drawing.Size(445, 201)
+        Me.DataGrid1.TabIndex = 0
+        '
+        'Add1
+        '
+        Me.Add1.BackColor = System.Drawing.Color.Black
+        Me.Add1.ForeColor = System.Drawing.Color.White
+        Me.Add1.Location = New System.Drawing.Point(168, 243)
+        Me.Add1.Name = "Add1"
+        Me.Add1.Size = New System.Drawing.Size(132, 31)
+        Me.Add1.TabIndex = 1
+        Me.Add1.Text = "Add"
+        Me.Add1.UseVisualStyleBackColor = False
+        '
+        'Delete1
+        '
+        Me.Delete1.BackColor = System.Drawing.Color.Black
+        Me.Delete1.ForeColor = System.Drawing.Color.White
+        Me.Delete1.Location = New System.Drawing.Point(322, 243)
+        Me.Delete1.Name = "Delete1"
+        Me.Delete1.Size = New System.Drawing.Size(132, 31)
+        Me.Delete1.TabIndex = 2
+        Me.Delete1.Text = "Delete"
+        Me.Delete1.UseVisualStyleBackColor = False
+        '
+        'Open1
+        '
+        Me.Open1.BackColor = System.Drawing.Color.Black
+        Me.Open1.ForeColor = System.Drawing.Color.White
+        Me.Open1.Location = New System.Drawing.Point(17, 243)
+        Me.Open1.Name = "Open1"
+        Me.Open1.Size = New System.Drawing.Size(132, 31)
+        Me.Open1.TabIndex = 3
+        Me.Open1.Text = "Open"
+        Me.Open1.UseVisualStyleBackColor = False
+        '
+        'Access
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(468, 289)
+        Me.Controls.Add(Me.Open1)
+        Me.Controls.Add(Me.Delete1)
+        Me.Controls.Add(Me.Add1)
+        Me.Controls.Add(Me.DataGrid1)
+        Me.Name = "Access"
+        Me.Text = "Access"
+        CType(Me.DataGrid1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGrid1 As DataGridView
+    Friend WithEvents Add1 As Button
+    Friend WithEvents Delete1 As Button
+    Friend WithEvents Open1 As Button
+End Class
